7a07f966cf048046697050c065be595ecef0bf65
thamash1998 <thamashwerosha@gmail.com>
2025-12-08 21:26:56 +0530

    Test Commit

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-Thamash
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "<h1>Hello, World!</h1><p>This is a simple Python web app.</p>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
